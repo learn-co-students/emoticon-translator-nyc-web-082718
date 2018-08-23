d24d19f7a145c437fbe592683a5d3af0e7018851
github username <github email address>
2018-08-23 04:04:48 -0400

    practice test-driven lab and learn to load YAML file

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,31 @@
-# require modules here
+require 'yaml'
 
-def load_library
-  # code goes here
+def load_library(file_path)
+  emoticon_library = {"get_meaning" => {}, "get_emoticon" => {} }
+  YAML.load_file(file_path).each do |meaning, array|
+    english, japanese = array
+    emoticon_library["get_emoticon"][english] = japanese
+    emoticon_library["get_meaning"][japanese] = meaning
+  end
+  emoticon_library
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+  emoticon_library = load_library(file_path)
+  result = emoticon_library["get_emoticon"][emoticon]
+  if result
+    result
+  else
+    "Sorry, that emoticon was not found"
+  end
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(file_path, emoticon)
+  library = load_library(file_path)
+    result = library["get_meaning"][emoticon]
+    if result
+      result
+    else
+      "Sorry, that emoticon was not found"
+    end
 end
